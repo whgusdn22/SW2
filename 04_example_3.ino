bbd472529c08b904331489b81e41412c837cc143
HyunWoo Cho <126188520+whgusdn22@users.noreply.github.com>
2023-09-20 15:53:17 +0900

    Update 04_example_3.ino

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -5,6 +5,9 @@ void setup() {
   Serial.begin(115200);
 }
 
+// L 이 깜빡이는 것이 목표기 때문에 L 부분만 살렸음.
+// Serial.print() 가 있으면 TX가 문자를 Serial 모니터로 보내고 있어 깜빡임
+
 void loop() {
   digitalWrite(PIN_LED, 0);
   delay(1000);
